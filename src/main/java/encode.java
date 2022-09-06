1bab5668891f8cbf408d8efea346ab45e092ad78
RoflPolarity <43727574+RoflPolarity@users.noreply.github.com>
2022-09-06 14:52:30 +0300

    read

--- src/main/java/encode.java
+++ src/main/java/encode.java
@@ -1,8 +1,19 @@
-import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class encode {
-    encode(){
-
+    File file;
+    encode() throws IOException {
+        read();
     }
-
+    private List<String> read() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        List<String> lst = new ArrayList<>();
+        String line;
+        while ((line = reader.readLine())!=null){
+            lst.add(line);
+        }
+        return lst;
+    }
 }
