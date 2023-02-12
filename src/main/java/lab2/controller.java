5b3fb0dde29dad5ffe35e633e86085fa4ebea9b5
RoflPolarity <ignis604@gmail.com>
2023-02-12 23:32:35 +0300

    Master initNew

--- src/main/java/lab2/controller.java
+++ src/main/java/lab2/controller.java
@@ -3,48 +3,44 @@ package lab2;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import lab1.decode;
-import lab1.encode;
-
 import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class controller {
 
-    @FXML
-    private Button decode;
+        @FXML
+        private Button SaveParameters;
 
-    @FXML
-    private Button encode;
+        @FXML
+        private Button decode;
 
-    @FXML
-    private TextField filePath;
+        @FXML
+        private Button encode;
 
-    @FXML
-    private Button obzor;
+        @FXML
+        private TextField filePath;
 
-    @FXML
-    private TextArea text;
+        @FXML
+        private Button gen_save;
 
-    @FXML
-    private Button updateParam;
+        @FXML
+        private Button obzor;
 
-    @FXML
-    private Button gen_save;
+        @FXML
+        private TextArea text;
+
+        @FXML
+        private Button updateParameters;
+
 
     @FXML
     void initialize(){
         final String[] FilePath = new String[1];
-        final String[] key = new String[1];
-        encode.setDisable(true);
         obzor.setOnAction(event -> {
             Task<Void> task = new Task<Void>(){
                 protected Void call() {
@@ -74,33 +70,32 @@ public class controller {
             };
                 new Thread(task).start();
             });
-            gen_save.setOnAction(event -> {
-                try {
-                    Gamma.saveParam();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            updateParam.setOnAction(event -> {
-                try {
-                    Gamma.saveParam();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
+
             encode.setOnAction(event -> {
-            try {
                 Gamma.crypt(filePath.getText());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+            });
+
+
             decode.setOnAction(event -> {
-                try {
-                    Gamma.crypt(filePath.getText());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                final String[] key = {""};
+                Task<Void> task = new Task<Void>(){
+                    protected Void call() {
+                        JFrame frame = new JFrame();
+                        JFileChooser JFC = new JFileChooser();
+                        JFC.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                        int result = JFC.showOpenDialog(frame);
+                        if (result == JFileChooser.APPROVE_OPTION) {key[0] = JFC.getSelectedFile().getAbsolutePath();}
+                        decoder decoder = new decoder(filePath.getText(),key[0]);
+                        try {
+                            decoder.decode();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return null;
+                    }
+                };
+                new Thread(task).start();
             });
         }
     }
